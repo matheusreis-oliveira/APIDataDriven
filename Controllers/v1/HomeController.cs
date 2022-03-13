fd72e321e4398eaea10dfd2223565b8765285536
Matheus dos Reis <mathreis1999@gmail.com>
2022-03-13 03:10:06 -0300

    try catch home controller

--- Controllers/v1/HomeController.cs
+++ Controllers/v1/HomeController.cs
@@ -16,18 +16,25 @@ namespace Backoffice.Controllers
             var employee = new UserModel { Id = 1, Username = "administrator", Password = "admin123", Role = "admin" };
             var manager = new UserModel { Id = 2, Username = "hardwork", Password = "ilovmyjob", Role = "employee" };
             var category = new CategoryModel { Id = 1, Title = "Hotbart Courses" };
-            var product = new ProductModel { Id = 1, Category = category, Title = "How to sell courses", Price = 2999.99M, Description = "How to sell your course in hotbart!"};
-            
-            context.Users.Add(employee);
-            context.Users.Add(manager);
-            context.Categories.Add(category);
-            context.Products.Add(product); 
-            await context.SaveChangesAsync();
+            var product = new ProductModel { Id = 1, Category = category, Title = "How to sell courses", Price = 2999.99M, Description = "How to sell your course in hotbart!" };
 
-            return Ok(new
+            try
             {
-                message = "Dados configurados"
-            });
+                context.Users.Add(employee);
+                context.Users.Add(manager);
+                context.Categories.Add(category);
+                context.Products.Add(product);
+                await context.SaveChangesAsync();
+
+                return Ok(new
+                {
+                    message = "Dados configurados"
+                });
+            }
+            catch
+            {
+                return (new { message = "Dados já estão configurados"});
+            }
         }
     }
 }
